c475641454a2c7cebe6654ba4860bcb931c7f5d9
Ham80234 <you@example.comHam80234@gmail.com>
2019-04-17 11:35:20 -0500

    added main

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,128 @@
 import 'package:flutter/material.dart';
-import 'package:myapp/entry.dart';
-import 'package:myapp/exerciselist.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(GettingStarted());
 
-class MyApp extends StatelessWidget {
+class GettingStarted extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'SQFLite DataBase Demo',
       theme: new ThemeData(
         primarySwatch: Colors.blue,
         brightness: Brightness.dark,
         
       ),
-      home: new HomeScreen(),
+    title: "SwoleMate",
+    home: new Scaffold(
+    appBar: new AppBar(
+        title: new Text("SwoleMate", style: TextStyle(fontSize: 16.0),),
+        
+        centerTitle: true,
+        actions: <Widget>[
+           Padding(
+             padding: const EdgeInsets.only(right: 16.0),
+             
+            ),
+          ],
+  elevation: 0.0,
+ 
+),
+ drawer: Drawer(
+   child: new ListView(
+     children: <Widget>[
+       new UserAccountsDrawerHeader(
+         accountName: new Text("John Smith"),
+         accountEmail: new Text("JohnSmith@gmail.com"),
+         decoration: new BoxDecoration(
+           image: new DecorationImage(
+             fit: BoxFit.fill,
+             image: new NetworkImage('https://cdn-images-1.medium.com/max/1200/1*lTEHN86OS67Nf-3-jUM73w.jpeg')
+           )
+         )
+       ),
+       new ListTile(
+         title: new Text("Home"),
+         trailing: new Icon(Icons.home)
+       ),
+        new ListTile(
+         title: new Text("Profile"),
+         trailing: new Icon(Icons.person)
+       ),
+        new ListTile(
+         title: new Text("Progress"),
+         trailing: new Icon(Icons.pie_chart)
+       )
+     ]
+   ),
+ ),
+      body: new MainContent(),
+      floatingActionButton: new FloatingActionButton(
+        child: new Icon(Icons.add),
+        backgroundColor: Colors.red,
+        onPressed: () {
+          print("Clicked");
+        },
+      ),
+      )
+    );
+    
+
+  }
+}
+
+class MainContent extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+      return new Scaffold(
+      body: Card(
+        child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const ListTile(
+            title: Text('Todays Workout'),
+           
+          ),
+          
+        ],
+       
+      ),
+     
+    ),
+  );
+  }
+}
+
+class WorkoutList extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+      return ListView.builder(
+        itemCount: 20,
+        itemBuilder: (context, RowNum) { 
+          return new Excercise();
+        }
+      );
+  }
+}
+
+class Excercise extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return new Center(
+   child: Card(
+    child: Column(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        const ListTile(
+          leading: Icon(Icons.album),
+          title: Text('Test Excercise'),
+          subtitle: Text('5 reps'),
+          
+        ),
+        
+      ],
+    ),
+  ),
     );
   }
 }
 
-class HomeScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text("Title"),
-      ),
-      body: new Center(child: new Text("Home Page")),
-      floatingActionButton: new FloatingActionButton(
-        child: new Icon(Icons.add),
-        backgroundColor: Colors.blue,
-        onPressed: () {
-          print("Clicked");
-          Navigator.push(
-            context,
-            new MaterialPageRoute(builder: (context) => new MyEmployeeList()),
-          );
-        },
-      ),
-    );
-  }
-}
+
